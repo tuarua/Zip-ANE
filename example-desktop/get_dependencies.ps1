e9c90f5d404aa470183c4dbda4a6860c110d4aa6
Eoin Landy <el111@yahoo.com>
2019-05-22 19:08:44 +0100

    fix for get_dependencies

--- example-desktop/get_dependencies.ps1
+++ example-desktop/get_dependencies.ps1
@@ -3,5 +3,5 @@ $FreSwiftVersion = "3.1.0"
 
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Swift-IOS-ANE/releases/download/FreSwiftVersion/FreSwift.ane?raw=true -OutFile "$currentDir\..\native_extension/ane/FreSwift.ane"
+Invoke-WebRequest -Uri https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/FreSwift.ane?raw=true -OutFile "$currentDir\..\native_extension/ane/FreSwift.ane"
 Invoke-WebRequest -Uri https://github.com/tuarua/Zip-ANE/releases/download/$AneVersion/ZipANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\ZipANE.ane"
