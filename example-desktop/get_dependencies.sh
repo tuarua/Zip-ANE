f9984942e5744afe041f6cbff02db17223a3a3fe
Tua Rua <info@tuarua.co.uk>
2019-05-31 20:46:39 +0100

    Update get_dependencies.sh

--- example-desktop/get_dependencies.sh
+++ example-desktop/get_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-AneVersion="1.1.0"
+AneVersion="1.2.0"
 FreSwiftVersion="3.1.0"
 
 wget -O ../native_extension/ane/FreSwift.ane https://github.com/tuarua/Swift-IOS-ANE/releases/download/$FreSwiftVersion/FreSwift.ane?raw=true
